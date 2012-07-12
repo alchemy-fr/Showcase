21301688b08a2ceef0c686e9f508d48a7ccd8411
Romain Neutron <imprec@gmail.com>
2012-07-12 13:42:03 +0200

    Fix tests

--- tests/Test/Provider/EntityManagerServiceProviderTest.php
+++ tests/Test/Provider/EntityManagerServiceProviderTest.php
@@ -3,15 +3,15 @@
 namespace Test\Provider;
 
 use PhraseanetSDK\Tools\Entity\Manager;
-use Alchemy\Showcase\Provider\EntityManager;
+use Alchemy\Showcase\Provider\EntityManagerServiceProvider;
 
-class EntityManagerTest extends \PHPUnit_Framework_TestCase
+class EntityManagerServiceProviderTest extends \PHPUnit_Framework_TestCase
 {
 
     public function testRegister()
     {
         $app = require __DIR__ . '/../../../src/Alchemy/Showcase/App.php';
-        $app->register(new EntityManager(), array(
+        $app->register(new EntityManagerServiceProvider(), array(
             'config.file_path' => __DIR__ . '/../../ressources/ini.json'
         ));
 
